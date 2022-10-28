d857a1d2ea257c7d9721c9fe5065b58ca706d7fe
Maja Dit <maja.ditrtova@outlook.com>
2022-10-28 18:09:55 +0100

    install html2canvas and test if downloading the card works

--- src/script.js
+++ src/script.js
@@ -1,5 +1,6 @@
 const nameForm = document.getElementById("name");
 const greetingForm = document.getElementById("greeting");
+const saveButton = document.getElementById("save-card-button");
 
 function submitName(event) {
   event.preventDefault();
@@ -12,5 +13,38 @@ function submitGreeting(event) {
   greeting.textContent = `${event.target.value}`;
 }
 
+function downloadURI(uri, name) {
+  let link = document.createElement("a");
+  link.download = name;
+  link.href = uri;
+  link.click();
+}
+// function saveCard(event) {
+//   event.preventDefault();
+//   let holidayCard = document.getElementById("card-display");
+// html2canvas(holidayCard).then(function(canvas) {
+//    holidayCard.appendChild(canvas);
+// });
+
+//   html2canvas(saveButton, {
+//     let myImage = canvas.toDataURL("image/png");
+//     downloadURI("data:" + myImage, "HappyHolidays.png");
+//   });
+// };
 nameForm.addEventListener("change", submitName);
 greetingForm.addEventListener("change", submitGreeting);
+// saveButton.addEventListener("click", saveCard);
+
+html2canvas(
+  (document.getElementById("save-card-button").onclick = function () {
+    const screenshotTarget = document.getElementById("card-display");
+    html2canvas(screenshotTarget).then((canvas) => {
+      const base64image = canvas.toDataURL("image/png");
+      var anchor = document.createElement("a");
+      anchor.setAttribute("href", base64image);
+      anchor.setAttribute("download", "my-image.png");
+      anchor.click();
+      anchor.remove();
+    });
+  })
+);
